72436106e1abb84c03036934ff82bd427457eea1
Nandini Shrivastav <shrivastav.n2002@gmail.com>
2026-03-12 17:08:16 +0530

    create tasks from the admin interface

---
+++ todo/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Task(models.Model):        #Base class for all Django models
+
+    title = models.CharField(max_length=200)
+
+
+    description = models.TextField()
+
+    completed = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
